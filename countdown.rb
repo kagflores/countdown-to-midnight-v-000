dda6c933fecd43f26789b14167d9368ffb1923c4
Karen Flores <karen.ag.flores@gmail.com>
2018-03-26 21:30:10 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -7,3 +7,12 @@ def countdown(time)
   end
   return "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(time)
+  while time>0
+    puts "#{time} SECOND(S)!"
+    time -= 1
+    sleep (1)
+  end
+  return "HAPPY NEW YEAR!"
+end
